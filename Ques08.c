e4b05eac8ede786b841d273b74b0121400fc1565
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2022-09-23 07:44:26 -1200

    Create Ques08.c

---
+++ Ques08.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+
+    for (i=1; i<=4; i++)
+    {
+        k = 0;
+        for (j=1; j<=7; j++)
+        {
+            if (j<=4-i)  
+                printf(" ");   
+                       
+        }
+
+        for (j=1; j<=4; j++)
+        {
+            if (j>=5-i)
+                printf("%d", ++k);
+        }
+
+       k--;
+       while (k)
+            printf("%d", k--);
+
+
+       printf("\n");
+
+    }
+
+    
+    printf("\n");
+    return 0;
+}
